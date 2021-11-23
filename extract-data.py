88fb57819ab23e045466b1c13560daa9f72c0f2f
Davide Mammarella <davide.mammarella@usi.ch>
2021-11-23 17:44:54 +0100

    Add classes/methods/functions extraction in dictionaries

--- extract-data.py
+++ extract-data.py
@@ -2,17 +2,29 @@ import os
 from fnmatch import fnmatch
 import ast
 
-
-# [extract data] extract names/comments of Python classes, methods, functions
 # Save the extracted info into a CSV file.
-# When available, extract also the comment line following the class, function or method declaration.
-
 # TODO (ASK) inner functions/methods can be skipped by just avoiding to trigger a recursive generic_visit call.
-# • To extract the comment line, you can:
-# • read the source file up to the line number of each entity; then, read the next lines until the declaration ends (i.e., ‘):’ or ‘->’ is found in Python): if the next line starts with ”””, in Python it is a comment line to be extracted
-# • use this function call: comment = ast.get_docstring(node)
-# • locate the following types of nodes: Str(s=‘comment’) (Python 3.7 and earlier) or
-# Constant(value=‘comment’, kind=None) (Python 3.8 and later)
+
+extracted_data = []
+file_path_and_name = None
+
+def add_data(name, file, line, type, comment):
+    """
+    Add a (Python) dictionary to a list of dictionaries.
+    A dictionary can contain a Python class, Python method or Python function.
+    :param name: name of the class/method/function
+    :param file: path from repository root of the class/method/function
+    :param line: line of the class/method/function declaration
+    :param type: class/method/function
+    :param comment: comment line of the class/method/function
+    """
+    data={}
+    data["name"] = name
+    data["file"] = file
+    data["line"] = line
+    data["type"] = type
+    data["comment"] = comment
+    extracted_data.append(data)
 
 class AstVisitor(ast.NodeVisitor):
     """
@@ -22,30 +34,33 @@ class AstVisitor(ast.NodeVisitor):
         """
         Visit a ClassDef node (Top Level Classes).
         :param node: ast node to visit
-        :return:
         """
         # node.name = names of the ClassDef entity (top level classes)
         # node.lineno = line number of the ClassDef entity
+        # comment = extract the comment associate to the ClassDef entity
         # TODO:
         #  exclude name starts with _,
         #  exclude name is "main",
         #  exclude name contains the word "test" (with all the case variants (TEST, teST, ecc)
-        # "file":None (cos
-        print(node.name, node.lineno)
+        global file_path_and_name, extracted_data
+        comment = ast.get_docstring(node)
+        add_data(node.name, file_path_and_name, node.lineno, "class", comment)
 
     def visit_FunctionDef(self, node: ast.FunctionDef):
         """
         Visit a FunctionDef node (Functions and Methods).
         :param node: ast node to visit
-        :return:
         """
         # node.name = names of the FunctionDef entity (functions and methods)
         # node.lineno = line number of the FunctionDef entity
+        # comment = extract the comment associate to the FunctionDef entity
         # TODO:
         #  exclude name starts with _,
         #  exclude name is "main",
         #  exclude name contains the word "test" (with all the case variants (TEST, teST, ecc)
-        print(node.name, node.lineno)
+        global file_path_and_name, extracted_data
+        comment = ast.get_docstring(node)
+        add_data(node.name, file_path_and_name, node.lineno, "functions", comment)
 
 
 def main():
@@ -61,10 +76,14 @@ def main():
     for path, subdirs, files in os.walk(root):
         for file in files:
             if fnmatch(file, pattern):
-                with open(os.path.join(path, file), 'r') as py_file:
+                global file_path_and_name
+                file_path_and_name = os.path.join(path, file)
+                with open(file_path_and_name, 'r') as py_file:
                     ast_of_py_file = ast.parse(py_file.read())
                     AstVisitor().visit(ast_of_py_file)
 
+    # dictionary to csv
+    print("Total number of row:", len(extracted_data))
 
 if __name__ == "__main__":
     main()
